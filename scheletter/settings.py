8249a542e750f0d485bfc528539ed961e3d11b59
Faturrachman Mochammad <dev@faturrachmanmochammad.id>
2023-05-20 19:33:28 +0700

    Add sign-request content

--- scheletter/settings.py
+++ scheletter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tlrc(n8&mt$_e@=_lrk*!m#08-aulc8_ix#4&&x#$wk&)nv088
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.14', 'localhost']
+ALLOWED_HOSTS = ['10.10.6.7','localhost']
 
 LOGOUT_REDIRECT_URL = 'index'
 LOGIN_REDIRECT_URL = 'dashboard'
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'integrations',
+    'letter',
 ]
 
 MIDDLEWARE = [
