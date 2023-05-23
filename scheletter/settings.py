77b97adf87da5f1d7e3b8cc6e09dbb07f937b809
Faturrachman Mochammad <dev@faturrachmanmochammad.id>
2023-05-23 17:05:35 +0700

    Add data render table  & fix integration settings

--- scheletter/settings.py
+++ scheletter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tlrc(n8&mt$_e@=_lrk*!m#08-aulc8_ix#4&&x#$wk&)nv088
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.10.6.7','localhost']
+ALLOWED_HOSTS = ['10.10.6.7','172.20.10.2','localhost']
 
 LOGOUT_REDIRECT_URL = 'index'
 LOGIN_REDIRECT_URL = 'dashboard'
