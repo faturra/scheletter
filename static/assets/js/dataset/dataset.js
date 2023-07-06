9494e3d9cb944f5b6b97caf80b5ccb75e22ded2a
Faturrachman Mochammad <dev@faturrachmanmochammad.id>
2023-07-06 13:23:58 +0700

    Add datatables students & employees

---
+++ static/assets/js/dataset/dataset.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('#tb_students').dataTable( {
+    "scrollX": true,
+    "pagingType": "simple",
+  } );
+});
+
+$(document).ready(function() {
+  $('#tb_employees').dataTable( {
+    "scrollX": true,
+    "pagingType": "simple",
+  } );
+});
