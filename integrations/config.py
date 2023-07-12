09496390c9584996ab4d8acd544b6b3f28b4956f
Faturrachman Mochammad <dev@faturrachmanmochammad.id>
2023-07-12 22:11:16 +0700

    Remove dropdown account name navbar

--- integrations/config.py
+++ integrations/config.py
@@ -1,6 +1,6 @@
 # from .logic_config import get_user_server_address, get_user_npsn, get_user_token
 
-server_address = 'dapodik.smpn162jakarta.sch.id'
-npsn = '20100766'
-api_token = 'ktuhMNbHJVY76ug'
+server_address = 'dapodik.smpn290jakarta.sch.id'
+npsn = '69980874'
+api_token = 'Wmr36w90UBMiout'
 base_url = 'http://' + server_address + ':1162/WebService/'
