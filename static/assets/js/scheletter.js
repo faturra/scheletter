d599d213fdc23142b07e69f8da70c4767f9e2429
Faturrachman Mochammad <dev@faturrachmanmochammad.id>
2023-07-20 00:17:08 +0700

    Reformating request queue content, etc

--- static/assets/js/scheletter.js
+++ static/assets/js/scheletter.js
@@ -8,12 +8,12 @@ document.onreadystatechange = function () {
         loadingBar.classList.add('loaded');
         setTimeout(function() {
           loadingBar.style.opacity = '0';
-        }, 500); // Mengubah opacity menjadi 0 setelah 500ms
-      }, 300); // Tambahkan kelas 'loaded' setelah 300ms
-    }, 500); // Tunda perubahan lebar selama 500ms
+        }, 500);
+      }, 300);
+    }, 500);
   } else {
     loadingBar.style.width = '0';
-    loadingBar.style.opacity = '1'; // Set opacity menjadi 1 saat halaman sedang dimuat
+    loadingBar.style.opacity = '1';
     loadingBar.classList.remove('loaded');
   }
 };
