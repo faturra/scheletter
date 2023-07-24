d841603567f3702d138a41667a2045dc2fae9b2a
Faturrachman Mochammad <dev@faturrachmanmochammad.id>
2023-07-25 00:31:22 +0700

    Add check animation student archive

--- scheletter/settings.py
+++ scheletter/settings.py
@@ -25,13 +25,14 @@ SECRET_KEY = 'django-insecure-tlrc(n8&mt$_e@=_lrk*!m#08-aulc8_ix#4&&x#$wk&)nv088
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.10.6.7',
-                 '192.168.1.74',
-                 '172.20.10.2',
-                 'a07817a5673f',
-                 'Faturras-M1.local',
-                 'localhost'
-                 ]
+ALLOWED_HOSTS = [
+    '10.10.6.7',
+    '192.168.1.74',
+    '172.20.10.2',
+    'a07817a5673f',
+    'Faturras-M1.local',
+    'localhost'
+    ]
 
 LOGOUT_REDIRECT_URL = 'index'
 LOGIN_REDIRECT_URL = 'dashboard'
