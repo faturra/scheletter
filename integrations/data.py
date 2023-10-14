b5de62069661b784ca41dc724c8dabd805c4169c
Faturrachman Mochammad <dev@faturrachmanmochammad.id>
2023-10-14 21:41:37 +0700

    Style: Card table last creatd letter dashboard

--- integrations/data.py
+++ integrations/data.py
@@ -2,7 +2,6 @@ import requests
 from django.core.cache import cache
 from .models import Integrations
 
-# Fungsi untuk mengambil data dari API
 def get_data_from_api(api_url, headers):
     try:
         response = requests.get(api_url, headers=headers)
@@ -12,7 +11,6 @@ def get_data_from_api(api_url, headers):
     except requests.exceptions.RequestException as e:
         return []
 
-# Fungsi untuk memperbarui data dari sumber API
 def update_api_data():
     integration = Integrations.objects.get()
     server_address = integration.server_address
@@ -31,21 +29,10 @@ def update_api_data():
     if dapodik_school_api:
         print('Dapodik School API Connection [OK].')
 
-    # Data Users
-    # dapodik_users_api = get_data_from_api(f'{base_url}getPengguna?npsn={npsn}', headers)
-    # if dapodik_users_api:
-    #     print('Dapodik Users API Connection [OK].')
-
     # Data Employees
     dapodik_employees_api = get_data_from_api(f'{base_url}getGtk?npsn={npsn}', headers)
     if dapodik_employees_api:
         print('Dapodik Employees API Connection [OK].')
-
-    # Data Learning Group
-    # dapodik_learning_group_raw = get_data_from_api(f'{base_url}getRombonganBelajar?npsn={npsn}', headers)
-    # dapodik_learning_group_api = [(item['nama'], item['nama']) for item in dapodik_learning_group_raw]
-    # if dapodik_learning_group_raw:
-    #     print('Dapodik Learning Group API Connection [OK].')
 
     # Data Students
     dapodik_students_api = get_data_from_api(f'{base_url}getPesertaDidik?npsn={npsn}', headers)
